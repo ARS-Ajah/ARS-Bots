c98c18703b2dbfb78dd11d317f1a19a82ac35b21
ARS Ajah Dev <kimininyipa@gmail.com>
2024-06-25 02:35:28 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,26 +1,36 @@
-const { Events } = require('discord.js');
+const { Events } = require("discord.js");
 
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
 
-		const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) {
-			console.error(`\x1b[91m[ARS Utility]\x1b[39m No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+    if (!command) {
+      console.error(
+        `\x1b[91m[ARS Utility]\x1b[39m No command matching ${interaction.commandName} was found.`
+      );
+      return;
+    }
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: '\x1b[91m[ARS Utility]\x1b[39m Error 500 - Internal Server Error', ephemeral: true });
-			} else {
-				await interaction.reply({ content: '\x1b[91m[ARS Utility]\x1b[39m Error 500 - Internal Server Error', ephemeral: true });
-			}
-		}
-	},
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content:
+            "[ARS Utility] Error 500 - Internal Server Error",
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content:
+            "[ARS Utility] Error 500 - Internal Server Error",
+          ephemeral: true,
+        });
+      }
+    }
+  },
 };
