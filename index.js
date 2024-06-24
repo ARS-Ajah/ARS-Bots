2977459dbf09cde19d82f09c53aceae507a7a1b7
ARS Ajah Dev <kimininyipa@gmail.com>
2024-06-25 02:23:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,19 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-require('dotenv').config()
+const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+require("dotenv").config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const functionsPath = path.join(__dirname, "functions");
+const functionFiles = fs
+  .readdirSync(functionsPath)
+  .filter((file) => file.endsWith(".js"));
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`\x1b[91m[ARS Utility]\x1b[39m The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
-
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+for (const file of functionFiles) {
+  const filePath = path.join(functionsPath, file);
+  const handler = require(filePath);
+  handler(client);
 }
 
 client.login(process.env.TOKEN);
