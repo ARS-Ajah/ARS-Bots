a98812343fc5dfeaa6e4f4ee10cc16cf0ec1043e
ARS Ajah Dev <kimininyipa@gmail.com>
2024-06-25 02:22:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-echo -e "\033[91m[ARS Utility]\033[39m Welcome to ARS Bots Installer!"; echo -e "\033[91m[ARS Utility]\033[39m Installing..."; npm init -y; npm i dotenv discord.js; npm i nodemon eslint --save-dev; echo -e "\033[91m[ARS Utility]\033[39m Starting..."; npm install; npm start;
+#!/bin/sh
+# Make Greeting
+echo -e "\033[91m[ARS Utility]\033[39m Welcome to ARS Bots Installer!"
+sleep 1
+echo -e "\033[91m[ARS Utility]\033[39m Installing..."
+# Install Package
+npm i dotenv discord.js
+npm i nodemon eslint --save-dev
+# Starting
+echo -e "\033[91m[ARS Utility]\033[39m Starting..."
+sleep 1
+# Actual Start
+npm install
