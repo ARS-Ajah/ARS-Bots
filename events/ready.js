7d73c56532b9bd2a62059ac6f9a932137b964b63
ARS Ajah Dev <kimininyipa@gmail.com>
2024-06-25 02:25:02 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,11 @@
-const { Events } = require('discord.js');
+const { Events } = require("discord.js");
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`\x1b[91m[ARS Utility] \x1b[39mReady! Logged in as ${client.user.tag}`);
-	},
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(
+      `\x1b[91m[ARS Utility] \x1b[39mReady! Logged in as ${client.user.tag}`
+    );
+  },
 };
